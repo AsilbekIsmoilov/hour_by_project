2b068b383a52f551173cbfc972da37a0d4afe2dd
Asilbek Ismoilov <asilbekismoilov212002@gmail.com>
2026-06-16 10:10:09 +0000

    Edit entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ if [ "${RUN_MIGRATIONS}" = "1" ]; then
   echo "Ensuring superuser (admin/123)..."
   python manage.py shell -c "from django.contrib.auth import get_user_model; U=get_user_model(); U.objects.filter(username='admin').exists() or U.objects.create_superuser('admin','','123')"
 
-  ( echo "daily_runner 2/2..."; python daily_runner.py ) &
+  ( echo "daily_runner 1/1..."; python daily_runner.py ) &
 fi
 
 exec "$@"
